15533d48c131c5c63ff9bc296de7f37ee80b3257
Bastien Libersa <bastien.libersa@gmail.com>
2017-02-07 16:20:15 +0200

    Initial commit

---
+++ install-zdelta-2.1.sh
@@ -0,0 +1,10 @@
+wget http://cis.poly.edu/zdelta/downloads/zdelta-2.1.tar.gz
+
+tar -xzvf zdelta-2.1.tar.gz
+
+cd zdelta-2.1
+
+sudo make && sudo make install
+
+sudo cp zdc /usr/local/bin/zdc
+sudo cp zdu /usr/local/bin/zdu
